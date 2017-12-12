e1aacece7dd69dbd7f9ec0f31c5611008df40707
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2017-12-12 18:50:25 -0300

    Implementacion de Logout Signed-off-by: Cristian Gonzalez <cs.gonzalez3@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,8 @@
 @extends('layouts.app')
 @section('titulo','Reservice')
-@section('login')
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth                    
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-                    <a href="{{ route('register') }}">Register</a>
-                @endauth
-            </div>
-        @endif
-    </div>
-@endsection
 @section('estilo-footer')
     <link rel="stylesheet" href="{{asset('css/footer-with-button-logo-white.css')}}">
 @endsection
 @section('contenido')
-    <h1></h1>
+    <h1>Inicio</h1>
 @endsection
